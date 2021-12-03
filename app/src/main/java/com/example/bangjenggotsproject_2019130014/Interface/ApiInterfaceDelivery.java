0bb37f958617cd48d13110933822681ec862a4e2
Calvin Leonardo <calvin.leo0818@gmail.com>
2021-12-04 00:02:09 +0700

    Perbaharuan Dine dan Delivery

--- app/src/main/java/com/example/bangjenggotsproject_2019130014/Interface/ApiInterfaceDelivery.java
+++ app/src/main/java/com/example/bangjenggotsproject_2019130014/Interface/ApiInterfaceDelivery.java
@@ -1,12 +1,17 @@
 package com.example.bangjenggotsproject_2019130014.Interface;
 
 import com.example.bangjenggotsproject_2019130014.Model.ModelDelivery;
+import com.example.bangjenggotsproject_2019130014.Model.ModelDine;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface ApiInterfaceDelivery {
     @POST("inputDelivery.php")
-    Call<ModelDelivery> delivery (@Body ModelDelivery modelDelivery);
+    Call<ModelDelivery> delivery(@Body ModelDelivery modelDelivery);
+
+    @GET("getAllDataDelivery.php")
+    Call<ModelDelivery> alldataddelivery();
 }
--- app/src/main/java/com/example/bangjenggotsproject_2019130014/Interface/ApiInterfaceDine.java
+++ app/src/main/java/com/example/bangjenggotsproject_2019130014/Interface/ApiInterfaceDine.java
@@ -4,9 +4,13 @@ import com.example.bangjenggotsproject_2019130014.Model.ModelDine;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface ApiInterfaceDine {
     @POST("inputDine.php")
     Call<ModelDine> dinein (@Body ModelDine modelDine);
+
+    @GET("getAllDataDine.php")
+    Call<ModelDine> alldatadine();
 }
